67550e21ae3b94707884ab3b4aa43ebaff0aca33
Hiep Nguyen <nxhiep.1996@gmail.com>
2017-10-30 13:28:36 +0700

    minor changes

--- cordova-supermusic.sh
+++ cordova-supermusic.sh
@@ -1,3 +1,5 @@
+cd ../
+export PATH=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin:"$PATH"
 cordova create Cordova com.hiep.supermusic SuperMusic
 cd Cordova
 cordova platform add android
@@ -5,6 +7,7 @@ cordova platform add ios
 cordova build
 cordova plugin add org.apache.cordova.console
 cordova plugin add org.apache.cordova.device
+cordova plugin add cordova-plugin-media
 cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
 cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-vibration.git
 cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-network-information.git
